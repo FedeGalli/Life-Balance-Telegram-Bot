160337074ad83f983132394847f5d2e5762479aa
FedeGalli <federicogalli96@gmail.com>
2026-09-25 11:21:06 +0200

    Updated docker build system

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,13 @@
-FROM python:alpine3.9
-WORKDIR .
+FROM python:3.12-slim-bookworm
+
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN python -m pip install --no-cache-dir -r requirements.txt
+
 COPY . .
-RUN pip install -r requirements.txt
+
 ENTRYPOINT ["python", "main.py"]
--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi -f $(docker images -aq)
-docker build . -t life-balance-image
-docker run --restart=always -e BOT_DATA_DIRECTORY=/data -v life-balance-data:/data life-balance-image
+#!/usr/bin/env sh
+set -eu
+
+IMAGE_NAME="life-balance-image"
+CONTAINER_NAME="life-balance-bot"
+
+docker build --tag "$IMAGE_NAME" .
+
+if docker container inspect "$CONTAINER_NAME" >/dev/null 2>&1; then
+    docker container rm --force "$CONTAINER_NAME"
+fi
+
+docker run \
+    --detach \
+    --name "$CONTAINER_NAME" \
+    --restart always \
+    --env BOT_DATA_DIRECTORY=/data \
+    --volume life-balance-data:/data \
+    "$IMAGE_NAME"
